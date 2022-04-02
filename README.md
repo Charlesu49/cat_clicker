9433a5d49eb1ec82848326e39cdf1fa30ea9d1bf
Charlesu49 <Charlesu49@gmail.com>
2022-04-02 23:02:33 +0100

    verion 1.1

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 This cat clicker project displays the image of a cat and increments the number of times is has been clicked on. it also spots a reset button that resets the counter to zero.
 
-Screenshot below;
+Screenshot of version 1.0 below;
 <img src="https://github.com/Charlesu49/cat_clicker/blob/master/screenshot.png" alt="screenshot">
 
 
+Verion 1.1
+New Project Requirements for Cat Clicker
+--Visuals--
+The application should display two cats. Each cat includes
+the cat's name
+a picture of the cat
+text showing the number of clicks
+The specifics of the layout do not matter, so style it however you'd like.
+
+--Interaction--
+The number of clicks should increment when each cat picture is clicked.
+N/B: Reset button is included as an extra.
+
+
+Screenshot of Version 1.1
+<img src="https://github.com/Charlesu49/cat_clicker/blob/master/screenshot_2.png" alt="screenshot of version 1.1">
+
+
+
+
+
 Image Source: 
-[UNSPLASH](https://www.unsplash.com "Visit Unsplash Website")
+Photo by [Onur Binay](https://unsplash.com/@onurbinay?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText ) on [UNSPLASH](https://www.unsplash.com)
--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 //declaring variables
-let count = 0;
-const counter = document.getElementById("count");
-const image = document.querySelector("img");
+
+//the variable storing the no of cat clicks
+let counter_1 = 0;
+let counter_2 = 0;
+
+//variable for counter elements for both cats
+const count1 = document.getElementById("count1");
+const count2 = document.getElementById("count2");
+
+//variable for the cat images
+const first_cat = document.getElementById("cat1");
+const second_cat = document.getElementById("cat2");
+
+//variable for the reset button
 const resetBtn = document.querySelector("button");
 
-image.addEventListener('click', function(){
-    count = count + 1;
-    counter.textContent = "Click Counter : " + count.toString();
+
+
+//functions
+first_cat.addEventListener('click', function(){
+    counter_1 = counter_1 + 1;
+    count1.textContent = "Click Counter : " + counter_1.toString();
+
+});
+
+second_cat.addEventListener('click', function(){
+    counter_2 = counter_2 + 1;
+    count2.textContent = "Click Counter : " + counter_2.toString();
 
 });
 
 
 //the below function resets the counter to 0
 resetBtn.addEventListener('click', function(){
-    count = 0;
-    counter.textContent = "Click Counter : " + count.toString();
+    counter_1 = 0;
+    counter_2 = 0;
+    count1.textContent = "Click Counter : " + counter_1.toString();
+    count2.textContent = "Click Counter : " + counter_2.toString();
 
 });
