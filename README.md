ab82e041ba831c374d5ee8760bc3dbd2b08c656f
Charlesu49 <Charlesu49@gmail.com>
2022-04-25 05:25:14 +0100

    version 1.2

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This cat clicker project displays the image of a cat and increments the number of times it is clicked on.
 
 Screenshot of version 1.0 below;
-<img src="https://github.com/Charlesu49/cat_clicker/blob/master/screenshot.png" alt="screenshot">
+<img src="https://github.com/Charlesu49/cat_clicker/blob/master/images/screenshot.png" alt="screenshot">
 
 # Verion 1.1
 ## New Project Requirements for Cat Clicker
@@ -17,8 +17,8 @@ Screenshot of version 1.0 below;
 - The number of clicks should increment when each cat picture is clicked.
 
 
-Screenshot of Version 1.1
-<img src="https://github.com/Charlesu49/cat_clicker/blob/master/screenshot_2.png" alt="screenshot of version 1.1">
+### Screenshot of Version 1.1
+<img src="https://github.com/Charlesu49/cat_clicker/blob/master/images/screenshot2.png" alt="screenshot of version 1.1">
 
 
 # Version 1.2 (Cat Clicker Premium) 
@@ -37,6 +37,8 @@ Screenshot of Version 1.1
 - should update to show the data for the selected cat.
 - The number of clicks in the cat area should be unique to each cat, and should increment when the cat's picture is clicked.
 
+### Screenshot of Version 1.2
+<img src="https://github.com/Charlesu49/cat_clicker/blob/master/images/screenshot3.png" alt="screenshot of version 1.2">
 
 
 
--- app.js
+++ app.js
@@ -1,41 +1,108 @@
-//declaring variables
-
-//the variable storing the no of cat clicks
-let counter_1 = 0;
-let counter_2 = 0;
-
-//variable for counter elements for both cats
-const count1 = document.getElementById("count1");
-const count2 = document.getElementById("count2");
-
-//variable for the cat images
-const first_cat = document.getElementById("cat1");
-const second_cat = document.getElementById("cat2");
-
-//variable for the reset button
-const resetBtn = document.querySelector("button");
 
 
+// local database to be used
+const cat_model = 
+{
+    cats: [
+        {
+            name : "Baby Cat",
+            cat_link : "https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFieSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+            cat_counter : 0,
+            id: 0
+        },
+        {
+            name : "Innocent Cattie",
+            cat_link : "https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTd8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+            cat_counter : 0,
+            id: 1
+        },
+        {
+            name : "Cutie Cattie",
+            cat_link : "https://images.unsplash.com/photo-1591621211041-a3d8b1a4e43a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmFieSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+            cat_counter : 0,
+            id: 2
+        },
+        {
+            name : "Angry Cat",
+            cat_link : "https://images.unsplash.com/photo-1588768987479-bcebeefb8a5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2F0dG98ZW58MHx8MHx8&w=1000&q=80",
+            cat_counter : 0,
+            id: 3
+        },
+        {
+            name : "Yellow Eyes",
+            cat_link : "https://images.unsplash.com/photo-1573592919522-cb19b002d543?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2F0JTIwaGVhZHxlbnwwfHwwfHw%3D&w=1000&q=80",
+            cat_counter : 0,
+            id: 4
+        }]
+};
 
-//functions
-first_cat.addEventListener('click', function(){
-    counter_1 = counter_1 + 1;
-    count1.textContent = "Click Counter : " + counter_1.toString();
+// get elements from the dom
+const count_statement = "Count: ";
+let current_cat = 0;
+let catNameElem = document.getElementById('cat_name');
+let catImageElem = document.getElementById('cat-img');
+let countElem = document.getElementById('count1');
+let resetBtn = document.getElementById('reset-btn');
+let catListElem = document.getElementById('cat_buttons'); // get a conatiner thats box all the cats (image)
 
+var cat_process = {
+    initialize: function() {
+        update(current_cat);
+        catList();
+    }
+};
+
+function update(i){
+
+    // let catId = document.getElementById()
+
+    catNameElem.textContent = cat_model.cats[i].name;
+    catImageElem.src = cat_model.cats[i].cat_link; // update src url
+    catImageElem.className = i; // update id
+   countElem.textContent = count_statement + cat_model.cats[i].cat_counter.toString();
+
+};
+
+function catList(){
+    
+    for (i = 0; i < cat_model.cats.length; i++) { // adds all button that help user navigate to a new cat
+       
+       let cat = cat_model.cats[i];
+
+        // make a new cat list item and set its text
+        elem = document.createElement('button');
+        elem.textContent = cat.name;
+        elem.id = cat.id;
+        catListElem.appendChild(elem);
+        };
+};
+
+cat_process.initialize(); // initialize app for the first time
+
+catImageElem.addEventListener('click', ()=>{ // this event listener function handles any cat click and handle the increment the counter
+    let indexClass = catImageElem.className; // get the class eg 1,2,3,4
+    indexClass = Number(indexClass); // convert the value to a number
+    cat_model.cats[indexClass].cat_counter++ // increment its value
+    countElem.textContent = count_statement + cat_model.cats[indexClass].cat_counter.toString(); // update increment on ui dom
+    
 });
 
-second_cat.addEventListener('click', function(){
-    counter_2 = counter_2 + 1;
-    count2.textContent = "Click Counter : " + counter_2.toString();
+// add an event listener to the button parent element
+catListElem.addEventListener('click', (e)=> {
+const clickedButtonId = e.target.id; // get the id of the button that was clicked
+update(clickedButtonId); // update value on the ui dom
+})
 
-});
+resetBtn.addEventListener('click', () => {
+    //Loop throungh all the cats array and reset its value to 0;
+    for (i = 0; i < cat_model.cats.length; i++) { //        
+        let cat = cat_model.cats[i];
+        cat.cat_counter = 0;
+    }; 
+    // call update() with index 0 to reset ui dom to its initial value when app loaded for the first
+    update(current_cat);
+})
 
 
-//the below function resets the counter to 0
-resetBtn.addEventListener('click', function(){
-    counter_1 = 0;
-    counter_2 = 0;
-    count1.textContent = "Click Counter : " + counter_1.toString();
-    count2.textContent = "Click Counter : " + counter_2.toString();
 
-});
+
